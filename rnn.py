5d8f6f6e7aa183e2fd2884c6e2f4291843b42691
MashyC <mashrurc02@gmail.com>
2019-08-06 19:26:57 -0400

    Add files via upload

--- rnn.py
+++ rnn.py
@@ -1,16 +1,9 @@
-# Recurrent Neural Network
-
-
-
-# Part 1 - Data Preprocessing
-
-# Importing the libraries
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
 # Importing the training set
-dataset_train = pd.read_csv('Google_Stock_Price_Train.csv')
+dataset_train = pd.read_csv('C:/Users/Mashrur/Desktop/PERSONAL/Programming/Deep_Learning_A_Z/Deep_Learning_A_Z/Volume 1 - Supervised Deep Learning/Part 3 - Recurrent Neural Networks (RNN)/Section 12 - Building a RNN/Recurrent_Neural_Networks/FB_TRAIN.csv')
 training_set = dataset_train.iloc[:, 1:2].values
 
 # Feature Scaling
@@ -21,17 +14,13 @@ training_set_scaled = sc.fit_transform(training_set)
 # Creating a data structure with 60 timesteps and 1 output
 X_train = []
 y_train = []
-for i in range(60, 1258):
-    X_train.append(training_set_scaled[i-60:i, 0])
+for i in range(3, 1565):
+    X_train.append(training_set_scaled[i-3:i, 0])
     y_train.append(training_set_scaled[i, 0])
 X_train, y_train = np.array(X_train), np.array(y_train)
 
 # Reshaping
 X_train = np.reshape(X_train, (X_train.shape[0], X_train.shape[1], 1))
-
-
-
-# Part 2 - Building the RNN
 
 # Importing the Keras libraries and packages
 from keras.models import Sequential
@@ -55,6 +44,14 @@ regressor.add(LSTM(units = 50, return_sequences = True))
 regressor.add(Dropout(0.2))
 
 # Adding a fourth LSTM layer and some Dropout regularisation
+regressor.add(LSTM(units = 50, return_sequences = True))
+regressor.add(Dropout(0.2))
+
+regressor.add(LSTM(units = 50, return_sequences = True))
+regressor.add(Dropout(0.2))
+
+
+# Adding a fifth LSTM layer and some Dropout regularisation
 regressor.add(LSTM(units = 50))
 regressor.add(Dropout(0.2))
 
@@ -65,34 +62,31 @@ regressor.add(Dense(units = 1))
 regressor.compile(optimizer = 'adam', loss = 'mean_squared_error')
 
 # Fitting the RNN to the Training set
-regressor.fit(X_train, y_train, epochs = 100, batch_size = 32)
-
-
-
-# Part 3 - Making the predictions and visualising the results
+regressor.fit(X_train, y_train, epochs = 50, batch_size = 16)
 
 # Getting the real stock price of 2017
-dataset_test = pd.read_csv('Google_Stock_Price_Test.csv')
+dataset_test = pd.read_csv('C:/Users/Mashrur/Desktop/PERSONAL/Programming/Deep_Learning_A_Z/Deep_Learning_A_Z/Volume 1 - Supervised Deep Learning/Part 3 - Recurrent Neural Networks (RNN)/Section 12 - Building a RNN/Recurrent_Neural_Networks/FB_TEST.csv')
 real_stock_price = dataset_test.iloc[:, 1:2].values
 
 # Getting the predicted stock price of 2017
 dataset_total = pd.concat((dataset_train['Open'], dataset_test['Open']), axis = 0)
-inputs = dataset_total[len(dataset_total) - len(dataset_test) - 60:].values
+inputs = dataset_total[len(dataset_total) - len(dataset_test) - 3:].values
 inputs = inputs.reshape(-1,1)
 inputs = sc.transform(inputs)
+
 X_test = []
-for i in range(60, 80):
-    X_test.append(inputs[i-60:i, 0])
+for i in range(3, 263):
+    X_test.append(inputs[i-3:i, 0])
 X_test = np.array(X_test)
 X_test = np.reshape(X_test, (X_test.shape[0], X_test.shape[1], 1))
 predicted_stock_price = regressor.predict(X_test)
 predicted_stock_price = sc.inverse_transform(predicted_stock_price)
 
 # Visualising the results
-plt.plot(real_stock_price, color = 'red', label = 'Real Google Stock Price')
-plt.plot(predicted_stock_price, color = 'blue', label = 'Predicted Google Stock Price')
-plt.title('Google Stock Price Prediction')
+plt.plot(real_stock_price, color = 'red', label = 'Real EURUSD Stock Price')
+plt.plot(predicted_stock_price, color = 'blue', label = 'Predicted EURUSD Stock Price')
+plt.title('EURUSD Price Prediction')
 plt.xlabel('Time')
-plt.ylabel('Google Stock Price')
+plt.ylabel('EURUSD Price')
 plt.legend()
 plt.show()
